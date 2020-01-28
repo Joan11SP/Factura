cccbf12257d0fad3faa3e7800b47920fab86d6a2
Bruja <sebaspe12345@gmail.com>
2020-01-28 12:54:14 -0500

    crud falta

--- controller/controller_user.js
+++ controller/controller_user.js
@@ -3,14 +3,22 @@ var users = require('../model/users'),
     router = express.Router(),
     validar = require('../utilities/utilities');
 
-router.post('/create', (req, res) => {
+router.post('/', (req, res) => {
+    users.find({ identification: body.identification }, (err, docs) => {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        res.status(200).json(docs)
+    })
+}).post('/create', (req, res) => {
     var body = req.body;
     if (validar.identification(body.identification) && validar.email(body.email) === true) {
         users.insertMany({
             identification: body.identification,
             email: body.email,
             password: body.password,
-            creation_date: new Date()
+            creation_date: Date.now()
         }, (err, docs) => {
             if (err) {
                 console.error(err);
@@ -19,7 +27,7 @@ router.post('/create', (req, res) => {
             res.status(200).json(docs)
         })
     }
-
+    res.json({ mensaje: 'Incorrect data' })
 }).post('/update', (req, res) => {
     var body = req.body;
     users.update({ identification: body.identification }, {
@@ -30,7 +38,7 @@ router.post('/create', (req, res) => {
             email: body.email,
             phones: body.phones,
             direction: body.direction,
-            update_date: new Date()
+            update_date: Date.now()
         }
     }, (err, docs) => {
         if (err) {
