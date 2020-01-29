64546ec94ececcd29ae329c6e4a2eb8ab0715a1c
Bruja <sebaspe12345@gmail.com>
2020-01-29 12:58:58 -0500

    update

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -36,33 +36,21 @@ const email = (valor) => {
     }
 }
 function ruc(number) {
-    var dto = number.length;
     var valor;
-    var acu = 0;
     if (number == "") {
-        //alert('No has ingresado ningún dato, porfavor ingresar los datos correspondientes.');
         return false;
     }
     else {
-        for (var i = 0; i < dto; i++) {
-            valor = number.substring(i, i + 1);
-            if (valor == 0 || valor == 1 || valor == 2 || valor == 3 || valor == 4 || valor == 5 || valor == 6 || valor == 7 || valor == 8 || valor == 9) {
-                acu = acu + 1;
+            valor = number.substring(10,13);
+            if ( valor == 001) {                
                 return true;
+            }else{
+                console.log("no es ruc")
             }
-        }
-        if (acu == dto) {
-            while (number.substring(10, 13) != 001) {
-               // alert('Los tres últimos dígitos no tienen el código del RUC 001.');
-                return false;
-            }
-            while (number.substring(0, 2) > 24) {
-                //alert('Los dos primeros dígitos no pueden ser mayores a 24.');
-                return false;
-            }
-        }
-        else {
-          //  alert("ERROR: Por favor no ingrese texto");
+        
+        if (number.substring(0, 2) > 24) {
+                console.log("fuera del numero de provincias")
+                return false;            
         }
     }
 }
